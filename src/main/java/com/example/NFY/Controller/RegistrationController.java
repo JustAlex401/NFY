5a405f4282a8492a1622bcfa0cc6c52a4e55c636
JustAlex401 <plashko99@mail.ru>
2020-05-03 13:29:50 +0300

    Initial commit

---
+++ src/main/java/com/example/NFY/Controller/RegistrationController.java
@@ -0,0 +1,50 @@
+package com.example.NFY.Controller;
+
+import com.example.NFY.domain.User;
+import com.example.NFY.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.jws.WebParam;
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/registration")
+    public String registration(Model model)
+    {
+        model.addAttribute("message", "");
+        return ("/registration");
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        if (!userService.addUser(user)) {
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code) {
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated) {
+            model.addAttribute("message", "User successfully activated");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+
+        return "login";
+    }
+}
---
+++ src/main/java/com/example/NFY/repos/UserRepo.java
@@ -0,0 +1,10 @@
+package com.example.NFY.repos;
+
+import com.example.NFY.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
+}
